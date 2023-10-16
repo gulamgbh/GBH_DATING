8eb675af09ad6369c3a212b6a0e68661bf309523
ravi-89669 <ravibirla2345@gmail.com>
2023-10-16 16:57:58 +0530

    update home page and nav bar code

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,24 +4,45 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary p-0 sticky-top" style={{backgroundColor:'rgb(25 22 22 / 69%)', zIndex:1}}>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary p-0 sticky-top" style={{ backgroundColor: 'rgb(25 22 22 / 69%)', zIndex: 1 }}>
         <div class="container-fluid">
           <div className="navbar-logo">
-           <a href="/"> <img src=".\images\official-removebg-preview.png" alt="not image" style={{ width: 200, height: 100 }}></img></a>
+            <a href="/"> <img src=".\images\official-removebg-preview.png" alt="not " style={{ width: 200, height: 100 }}></img></a>
           </div>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-              <li class="nav-item">
-                <a class="nav-link active " aria-current="page" href="/Products">Products</a>
+          <div className="collapse navbar-collapse menu fs-5 " id="navbarSupportedContent ">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 fw-bold">
+            <li className="nav-item dropend">
+                <a href="/products" className="nav-link dropdown" >
+                  Products
+                </a>
+                <ul className="dropdown-menu fs-5 fw-bold">
+                  <li><a className="dropdown-item" href="#">Community Gidellines</a></li>
+                  <li><a className="dropdown-item" href="#">Saftey Tipes</a>
+                  <ul style={{listStyle:"none"}}>
+                  <li><a className="dropdown-item" href="#">Saftey & Policy</a></li>
+                  <li><a className="dropdown-item" href="#">Saftey & Reporting</a></li>
+                  <li><a className="dropdown-item" href="#">SubScription Tiers</a></li>
+                  </ul></li>
+                  
+                </ul>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="/learn">Learn</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/Safety">Safety</a>
+              <li className="nav-item dropend">
+                <a  href="/safety" className="nav-link dropdown"  >
+                  Saftey
+                </a>
+                <ul className="dropdown-menu fs-5 fw-bold">
+                  <li><a className="dropdown-item" href="#">Community Gidellines</a></li>
+                  <li><a className="dropdown-item" href="#">Saftey Tipes</a></li>
+                  <li><a className="dropdown-item" href="#">Saftey & Policy</a></li>
+                  <li><a className="dropdown-item" href="#">Saftey & Reporting</a></li>
+                  <li><a className="dropdown-item" href="#">SubScription Tiers</a></li>
+                </ul>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="/Support">Support</a>
@@ -31,12 +52,13 @@ const Header = () => {
               </li>
             </ul>
             <div className="col">
-              <div className="d-flex float-right text-white " role="search">
+              <div className="d-flex float-end text-white mx-4 " role="search">
                 <h3 className=" pr-4">Language</h3>
-                <a href="/login" ><button type="button" className ="btn btn-light rounded-pill px-4 login-btn">Log in</button></a>
+                <a href="/login" ><button type="button" className="btn btn-light rounded-pill px-4 login-btn mx-4">Log in</button></a>
               </div>
             </div>
           </div>
+
         </div>
       </nav>
     </>
